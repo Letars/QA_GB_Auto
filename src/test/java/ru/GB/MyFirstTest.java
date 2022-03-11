920ea887bfbc218e89924520f0637aad56b7d825
Letars <e.sichevskaya@inbox.ru>
2022-03-11 17:10:11 +0300

    22

--- src/test/java/ru/GB/MyFirstTest.java
+++ src/test/java/ru/GB/MyFirstTest.java
@@ -18,13 +18,13 @@ public class MyFirstTest {
 
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName", "Pixel_4_API_30_233");
-        capabilities.setCapability("platformVersion", "11.0");
+        capabilities.setCapability("platformVersion", "11");
         capabilities.setCapability("udid", "emulator-5554");
         capabilities.setCapability("automationName", "UiAutomator2");
 //      Проверьте актуальность пути до приложения на своём компьютере.
         capabilities.setCapability("app", "/Users/robot/Downloads/Android-NativeDemoApp-0.2.1.apk");
 
-        MobileDriver driver = new AndroidDriver(new URL("<http://127.0.0.1:4723/wd/hub>"), capabilities);
+        MobileDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         //      Нажимаем Login в меню
         MobileElement loginMenuButton = (MobileElement) driver.findElementByAccessibilityId("Login");
         loginMenuButton.click();
@@ -34,7 +34,7 @@ public class MyFirstTest {
         loginButton.click();
         Thread.sleep(2000);
 //      Проверяем текст ошибки.
-        MobileElement errorText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\\"Login-screen\\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView[1]");
+        MobileElement errorText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Login-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView[1]");
         Assert.assertEquals(errorText.getText(), "Please enter a valid email address");
     }
 }
